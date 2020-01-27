89ea051df0ce59fe7c63aa773af8e799981c4e82
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-27 21:30:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -31,12 +31,12 @@ end
 
 
 def printer(array)
-  batch_badge_creator(array).each do |id|
-    puts id
+  batch_badge_creator(array).each do |name|
+    puts name
   end 
   
-  assign_rooms(array).each do |id|
-    puts id 
+  assign_rooms(array).each do |room|
+    puts room 
   end
 end 
 
