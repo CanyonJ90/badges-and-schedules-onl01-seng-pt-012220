bc6d5b0367fe0b54602156b38dcbb469692c4381
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-27 20:40:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,15 @@
 
-speakers= [ Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-
-
-
+speakers= [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 
 def badge_maker(name)
     return "Hello, my name is #{name}."
 end
+
+def badge_maker(name)
+  name = []
+  returns.each do "Hello my name is #{name}."
+  
+end
+
 
