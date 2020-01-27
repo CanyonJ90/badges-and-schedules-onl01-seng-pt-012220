71609e305af1be9746f4b8703cc776fc333e10ca
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-27 21:28:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(attendees)
   rooms = []
   counter = 1 
   attendees.each do |name|
-   rooms.push("Hello, #{name}! You'll be assigned to room #{counter}! ")
+   rooms.push("Hello, #{name}! You'll be assigned to room #{counter}!")
    counter += 1
  end
  return rooms
